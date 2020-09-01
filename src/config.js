18c925b32783898af10d7fabf4125340d5357698
William Brawner <me@wbrawner.com>
2020-09-01 18:25:49 +0000

    Fix MySQL host/port config

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const basicAuthConfig = {
 export const dbConfig = {
     connectionLimit: process.env.DB_POOL_SIZE || 10,
     host: process.env.DB_HOST || 'localhost',
-    host: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'flayre',
     password: process.env.DB_PASSWORD || 'flayre',
     database: process.env.DB_NAME || 'flayre'
